4fdd2691f9b0b0a48b38b9fe01fe42fcead43a5f
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-09-08 02:46:33 +0530

    create schema

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
+    category = Column(String, nullable=False)
     tags = Column(ARRAY(String))
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
--- app/routes/blog.py
+++ app/routes/blog.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, APIRouter, Depends
-from .. import models
+from .. import models, schema
 from ..database import get_db
 from sqlalchemy.orm import Session
+from typing import List
 
 router = APIRouter(
     prefix="/posts",
@@ -9,7 +10,7 @@ router = APIRouter(
 )
 
 # method to get all post
-@router.get("/")
+@router.get("/", response_model=List[schema.Post])
 def get_blog_posts(db:Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
