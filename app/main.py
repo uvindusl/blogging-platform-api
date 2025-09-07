63df2a28d4e8ffbfbe8e27071f34a84ec6ce4f41
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-09-08 02:27:04 +0530

    connect database and created tables

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
 from .routes import blog
+from .database import engine
+from . import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 
 app.include_router(blog.router)
 
---
+++ app/models.py
@@ -0,0 +1,16 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+
+class Posts(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    tags = Column(ARRAY(String))
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
