cedf77bf8f1c3191ab5e8ce359f26acd90b7bc4f
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-09-08 18:43:08 +0530

    get one post & delete post endpoints done

--- app/routes/blog.py
+++ app/routes/blog.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Depends, status
+from fastapi import FastAPI, APIRouter, Depends, status, HTTPException, Response
 from .. import models, schema
 from ..database import get_db
 from sqlalchemy.orm import Session
@@ -25,3 +25,25 @@ def create_posts(post: schema.PostCreate, db:Session = Depends(get_db)):
     db.refresh(new_post)
 
     return new_post
+
+# method to get only one post
+@router.get('/{id}',response_model=schema.Post)
+def get_one_post(id: int,db:Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found")
+    
+    return post
+        
+# method to delete a post
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db:Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Post with id: {id} not found')
+
+    post_query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
